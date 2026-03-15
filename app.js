8a082fcf677b9c30e6c7edf263ec519340dc48ca
Indrajeet230206 <nadargeindrajeet@gmail.com>
2026-03-15 17:56:23 +0530

    Add error handling middleware and async error wrapper

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing = require("./models/listing.js")
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -31,10 +33,10 @@ app.get("/", (req, res) => {
 
 
 //index route
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-})
+}));
 
 //new route
 app.get("/listings/new", (req, res) => {
@@ -42,39 +44,42 @@ app.get("/listings/new", (req, res) => {
 });
 
 //create route
-app.post("/listings", async (req, res) => {
+app.post("/listings", wrapAsync(async (req, res, next) => {
     // let {title, description, image, price, country, location} = req.body;
+    if(!req.body.listing.title) {  // this will work for hopscotch, but now it will not work bcause we have used required in form
+        throw new ExpressError(400, "send valid data for listing")
+    };
     const newlisting = new Listing(req.body.listing);
     await newlisting.save();
     res.redirect("/listings");
-});
+}));
 
 //show route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
-});
+}));
 
 //edit route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", {listing});
-});
+}));
 
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //delete route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-});
+}));
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
@@ -90,6 +95,12 @@ app.delete("/listings/:id", async (req, res) => {
 //     console.log("sample was saved");
 //     res.send("successful test");
 // });
+
+app.use((err, req, res, next) => {
+    let {statusCode = 500, message="something went wrong"} = err;
+    res.status(statusCode).render("error.ejs", {message});
+    // res.status(statusCode).send(message);
+});
 
 app.listen(1234, () => {
     console.log("server is listening on port 1234");
