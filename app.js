a5193862088d58b176ca6c1609be7929d52c454b
Indrajeet230206 <nadargeindrajeet@gmail.com>
2026-03-15 21:59:55 +0530

    validate schema with joi

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema");
 
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -31,6 +32,15 @@ app.get("/", (req, res) => {
     res.send("hi, i am root");
 });
 
+const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+}
 
 //index route
 app.get("/listings", wrapAsync(async (req, res) => {
@@ -44,11 +54,8 @@ app.get("/listings/new", (req, res) => {
 });
 
 //create route
-app.post("/listings", wrapAsync(async (req, res, next) => {
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
     // let {title, description, image, price, country, location} = req.body;
-    if(!req.body.listing.title) {  // this will work for hopscotch, but now it will not work bcause we have used required in form
-        throw new ExpressError(400, "send valid data for listing")
-    };
     const newlisting = new Listing(req.body.listing);
     await newlisting.save();
     res.redirect("/listings");
@@ -68,7 +75,8 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     res.render("listings/edit.ejs", {listing});
 }));
 
-app.put("/listings/:id", wrapAsync(async (req, res) => {
+//Update route
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
